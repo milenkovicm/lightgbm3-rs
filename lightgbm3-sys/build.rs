c48099ff707e69d70113920e946894d6358e9f72
Marko Milenković <milenkovicm@users.noreply.github.com>
2024-03-21 20:12:39 +0000

    Update deps to latest and add support for 4.3.0

--- lightgbm3-sys/build.rs
+++ lightgbm3-sys/build.rs
@@ -39,7 +39,7 @@ fn main() {
     let cfg = cfg
         .profile("Release")
         .uses_cxx11()
-        .cxxflag("-std=c++11")
+        .cxxflag("-std=c++17") // updated to c++17 from 11
         .define("BUILD_STATIC_LIB", "ON");
     #[cfg(not(feature = "openmp"))]
     let cfg = cfg.define("USE_OPENMP", "OFF");
@@ -52,8 +52,17 @@ fn main() {
     // bindgen build
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_args(&["-x", "c++", "-std=c++11"])
+        .clang_args(&["-x", "c++", "-std=c++17"]) // updated to c++17 from 11
         .clang_arg(format!("-I{}", lgbm_root.join("include").display()))
+        // had to disable bunch of things to make it running at 4.3
+        .opaque_type("std::.*")
+        .blocklist_type("iterator")
+        .blocklist_type("type_")
+        .blocklist_type("rep")
+        .blocklist_item("std::value")
+        .blocklist_item(".*__max")
+        .blocklist_item(".*__min")
+        // END had to disable bunch of things to make it running at 4.3
         .generate()
         .expect("Unable to generate bindings");
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
